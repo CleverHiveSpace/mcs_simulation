152eb061818680e39db97ad8a4ed4b3e2afed67f
Szymon Bednorz <dsonyyy@gmail.com>
2025-06-17 15:36:18 +0200

    feat(edge): update topic list to republish

--- src/mcs_ros2/mcs_ros2/telemetry_listener.py
+++ src/mcs_ros2/mcs_ros2/telemetry_listener.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import Imu, JointState, PointCloud2
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from rosidl_runtime_py import message_to_ordereddict
 from typing import Optional, Any, Dict, List
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Vector3Stamped
 
 
 class TelemetrySubscriber(Node):
@@ -20,30 +22,40 @@ class TelemetrySubscriber(Node):
             self.get_logger().error(f"Failed to connect to socket.io server: {e}")
 
         imu_sub: Subscriber = Subscriber(self, Imu, '/imu_broadcaster/imu')
-        pcl_sub: Subscriber = Subscriber(self, PointCloud2, '/rosbot/laser/point_cloud')
-        joint_sub: Subscriber = Subscriber(self, JointState, '/joint_states')
+        odom_sub: Subscriber = Subscriber(self, Odometry, '/odometry/filtered')
+        north_vector_sub: Subscriber = Subscriber(self, Vector3Stamped, '/rosbot/imu_compass/north_vector')
 
         self.ts: ApproximateTimeSynchronizer = ApproximateTimeSynchronizer(
-            [imu_sub, pcl_sub, joint_sub], queue_size=10, slop=0.1
+            [imu_sub, odom_sub, north_vector_sub], queue_size=10, slop=0.1
         )
         self.ts.registerCallback(self.synced_callback)
 
-    def synced_callback(self, imu_msg: Imu, pcl_msg: PointCloud2, joint_msg: JointState) -> None:
+    def synced_callback(self, 
+                        imu_msg: Imu, 
+                        odom_msg: Odometry, 
+                        north_vector_msg: Vector3Stamped) -> None:
         imu_data: Dict[str, Any] = message_to_ordereddict(imu_msg)
-        pcl_data: Dict[str, Any] = message_to_ordereddict(pcl_msg)
+        
+        north_vector_data: Dict[str, Any] = message_to_ordereddict(north_vector_msg)
 
-        joint_data: Dict[str, Any] = {
-            'timestamp': f"{joint_msg.header.stamp.sec}.{joint_msg.header.stamp.nanosec}",
-            'name': joint_msg.name,
-            'position': list(joint_msg.position),
-            'velocity': list(joint_msg.velocity),
-            'effort': self.filter_nan_values(list(joint_msg.effort))
+        odom_data: Dict[str, Any] = {
+            'timestamp': f"{odom_msg.header.stamp.sec}.{odom_msg.header.stamp.nanosec}",
+            'position': {
+                'longitude': odom_msg.pose.pose.position.x,
+                'latitude': odom_msg.pose.pose.position.y,
+                'altitude': odom_msg.pose.pose.position.z
+            },
+            'orientation': {
+                'roll': odom_msg.pose.pose.orientation.x,
+                'pitch': odom_msg.pose.pose.orientation.y,
+                'yaw': odom_msg.pose.pose.orientation.z
+            }
         }
 
         synchronized_data: Dict[str, Any] = {
             'imu': imu_data,
-            'point_cloud': pcl_data,
-            'joint_states': joint_data  
+            'odom': odom_data,
+            'north_vector': north_vector_data
         }
         
         current_ros_time = self.get_clock().now().to_msg()
